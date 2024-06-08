70e30fab92d0a6a30cbe803d7f27fa29e0dc7dfa
Evgeny <efonarev@mail.ru>
2024-06-08 13:34:54 +0300

    tenth commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+
 function openModalWindow(){
-    alert('Вы можете помочь природе перечислив сумму через СБП на тел.+7(916)116-19-18. Огромное Вам спасибо за посильный вклад! Вместе мы победим!');
+    let modalWin = document.getElementById('myModal');
+    let closeX = document.getElementById('closeX');
+    modalWin.style.opacity = '1';
+    modalWin.style['pointer-events'] = 'auto';
+    modalWin.style['z-index'] = '1';
+    closeX.style.cursor = 'pointer';
+    // alert('Это модальное окно\n\
+    // Вы можете помочь природе перечислив сумму через СБП на тел.+7(916)116-19-18.\
+    // \nОгромное Вам спасибо за посильный вклад!\
+    // \nВместе мы победим!');
+};
+function closeModalWin(){
+    let closeX = document.getElementById('closeX');
+    let modalWin = document.getElementById('myModal');
+    modalWin.style.opacity = '0';
+    modalWin.style['pointer-events'] = 'none';
+    closeX.style.cursor = 'default';
+    modalWin.style['z-index'] = '-1';
 }
